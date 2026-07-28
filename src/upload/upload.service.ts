ef5b766a508837f9a281b47369bfa9419d7c87ce
Daniel Acosta Herrera <danieldavidacostaherrera@gmail.com>
2026-07-28 09:34:50 -0400

    Corregido upload

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import cloudinary from '../config/cloudinary.config';
+import streamifier from 'streamifier';
 
 @Injectable()
 export class UploadService {
-  async uploadImage(filePath: string): Promise<string> {
-    const result = await cloudinary.uploader.upload(filePath, {
-      folder: 'anuncios',
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: 'anuncios' },
+        (error, result) => {
+          if (error) {
+            return reject(error);
+          }
+          if (!result) {
+            return reject(new Error('No se recibió resultado de Cloudinary'));
+          }
+          resolve(result.secure_url);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
     });
-    return result.secure_url;
   }
 }
+
+
+
